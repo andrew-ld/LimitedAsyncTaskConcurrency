8357661dfbb3b4b1dad590cabe4dc9041c352fff
andrew-ld <43882924+andrew-ld@users.noreply.github.com>
2022-10-30 17:00:39 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -7,7 +7,7 @@ class LimitedAsyncTaskConcurrency:
     __slots__ = ("_parallelism", "_tasks_queue", "_tasks", "_consumer")
 
     _tasks: dict[asyncio.Task, asyncio.Future]
-    _tasks_queue: asyncio.Queue[tuple[typing.Callable, asyncio.Future, tuple[...]]]
+    _tasks_queue: asyncio.Queue[tuple[typing.Callable, asyncio.Future, tuple[typing.Any, ...]]]
     _parallelism: int
     _consumer: asyncio.Task
 
@@ -24,16 +24,23 @@ class LimitedAsyncTaskConcurrency:
             has_get_from_queue_task = len(self._tasks) < self._parallelism
 
             if has_get_from_queue_task:
-                completed_tasks, _ = await asyncio.wait((get_from_queue_task,) | self._tasks.keys(), return_when=asyncio.FIRST_COMPLETED)
+                awaitable_tasks = (get_from_queue_task,) | self._tasks.keys()
             else:
-                completed_tasks, _ = await asyncio.wait(self._tasks.keys(), return_when=asyncio.FIRST_COMPLETED)
+                awaitable_tasks = self._tasks.keys()
+
+            completed_tasks, _ = await asyncio.wait(awaitable_tasks, return_when=asyncio.FIRST_COMPLETED)
 
             if has_get_from_queue_task and get_from_queue_task in completed_tasks:
                 completed_tasks.remove(get_from_queue_task)
 
                 new_task_function, new_task_sentinel, new_task_arguments = get_from_queue_task.result()
-                new_task_coroutine = asyncio.create_task(new_task_function(*new_task_arguments))
-                self._tasks[new_task_coroutine] = new_task_sentinel
+
+                try:
+                    new_task_coroutine = asyncio.create_task(new_task_function(*new_task_arguments))
+                except Exception as task_create_error:
+                    new_task_sentinel.set_exception(task_create_error)
+                else:
+                    self._tasks[new_task_coroutine] = new_task_sentinel
 
                 get_from_queue_task = asyncio.create_task(self._tasks_queue.get())
 
@@ -49,6 +56,3 @@ class LimitedAsyncTaskConcurrency:
         sentinel = asyncio.get_running_loop().create_future()
         await self._tasks_queue.put((function, sentinel, arguments))
         return sentinel
-
-    def __del__(self):
-        self._consumer.cancel()
